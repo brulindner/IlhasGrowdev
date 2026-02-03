8462ed4af1bb7f375482d92782377ef383144e2b
Bruna Lais Lindner <brunalaislindner@outlook.com>
2026-02-03 20:45:45 -0300

    script

---
+++ script.js
@@ -0,0 +1,25 @@
+let valorSaque = parseInt(prompt("Informe qual valor deseja sacar: (Lembrando, que só é aceito o saque de valores inteiros)"))
+
+
+if (isNaN(valorSaque) || valorSaque <= 0){
+    alert("Valor inválido, por favor informe um número inteiro positivo!")
+}
+else{
+    const notasDisponiveis = [50, 10, 5, 1];
+
+    notasDisponiveis.sort((a, b) => b - a);
+
+    console.log(`Saque solicitado: GC$ ${valorSaque},00`)
+
+    notasDisponiveis.forEach(nota => {
+        let quantidadeNotas = Math.floor(valorSaque / nota);
+
+        valorSaque = valorSaque % nota;
+
+        if(quantidadeNotas > 0){
+        console.log(`GC$ ${nota},00 -> ${quantidadeNotas}`);
+        }
+})
+}
+
+
